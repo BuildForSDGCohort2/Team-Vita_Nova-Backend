42c47323155de78ed3761dfa0ba1c13a32a70269
Awesome Kelly <e.oboh@semicolon.africa>
2020-09-13 21:50:54 +0100

    User Onboarding

--- vitaNova/settings/base.py
+++ vitaNova/settings/base.py
@@ -32,7 +32,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', ]
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '0.0.0.0']
 
 # Application definition
 

b95d55c33585646c82bec498a4618c293aef6fc5
Awesome Kelly <e.oboh@semicolon.africa>
2020-09-18 08:20:57 +0100

    Distributor and Sender endpoints

--- api/tests.py
+++ api/tests.py
@@ -9,7 +9,9 @@ from api.views import ProfileViewSet, UserReviewViewSet
 
 class TestUserProfile(TestCase):
 
-    """Test module for the UserProfile ViewSet"""
+    """
+    Test module for the UserProfile ViewSet
+    """
 
     def setUp(self):
         self.client = APIClient()
@@ -78,7 +80,9 @@ class TestUserProfile(TestCase):
 
 class TestUserReviewViewSet(TestCase):
 
-    """test modules for User Review"""
+    """
+    Test modules for User Review
+    """
 
     def setUp(self):
         self.client = APIClient()

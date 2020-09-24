2be2299733b5ac7555cb78aeb0c7cea0411372dd
rapha18th <rairorr@gmail.com>
2020-09-24 17:27:35 +0200

    update admin

--- api/admin.py
+++ api/admin.py
@@ -10,15 +10,11 @@ class UserAdmin(admin.ModelAdmin):
 
 
 class UserReviewAdmin(admin.ModelAdmin):
-    list_display = ('id', 'created', 'users', 'type_of_review', 'comment', 'reviewer', 'rating')
+    list_display = ('id', 'created', 'user', 'type_of_review', 'comment', 'reviewer', 'rating')
 
 class ChatRoomAdmin(admin.ModelAdmin):
-    list_display = ('id', 'uuid', 'user', 'notice_by_users', 'last_interaction')
-
-class MessageAdmin(admin.ModelAdmin):
-    list_display = ('id', 'created', 'user', 'chatroom', 'read_by_users', 'content')
+    pass
 
 admin.site.register(am.AppUser, UserAdmin)
 admin.site.register(am.UserReview, UserReviewAdmin)
 admin.site.register(am.ChatRoom, ChatRoomAdmin)
-admin.site.register(am.Message, MessageAdmin)

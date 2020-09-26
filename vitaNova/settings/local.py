50efb7b1904067928e0db7fc6fbe6edf2f9255f0
Awesome Kelly <e.oboh@semicolon.africa>
2020-09-26 08:32:32 +0100

    working on user verification

--- vitaNova/settings/local.py
+++ vitaNova/settings/local.py
@@ -1,6 +1,8 @@
-from dj_database_url import parse as db_url
-
 from .base import *
+
+from dj_database_url import parse as db_url
+from datetime import timedelta
+
 
 DEBUG = True
 CORS_ORIGIN_ALLOW_ALL = True
